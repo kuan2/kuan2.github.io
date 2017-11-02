ac534af4396da9ab9d740c4370ff28cb261bc633
luzhongkuan <zhongkuan.lu@vhuomai.com>
2017-11-02 16:53:40 +0800

    update home page

---
+++ pages.js
@@ -0,0 +1,9 @@
+let pages = [
+  {name: '代码雨', href: 'codeRain'},
+  {name: '烟花', href: 'fireworks'},
+  {name: '花朵', href: 'flower'},
+  {name: '跳动的心', href: 'heart'},
+  // {name: '抽奖', href: 'random'},
+  {name: '表白', href: 'sayLove'},
+  {name: '智能机器人', href: 'tuling'}
+];
