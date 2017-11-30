46d2c9e45ad2b7de37037f63a808cd72f0dfeb44
luzhongkuan <zhongkuan.lu@vhuomai.com>
2017-11-30 22:14:48 +0800

    update

--- pages.js
+++ pages.js
@@ -3,7 +3,7 @@ let pages = [
   {name: '烟花', href: 'fireworks'},
   {name: '花朵', href: 'flower'},
   {name: '跳动的心', href: 'heart'},
-  // {name: '抽奖', href: 'random'},
+  {name: '抽奖', href: 'random'},
   {name: '表白', href: 'sayLove'},
   {name: '智能机器人', href: 'tuling'},
   {name: '白云', href: 'cloud'},
